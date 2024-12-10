3b009b3e4b5ce6b9444d6845b4d2d59fbe0042a4
Dylan (Jeongtae Kim) <dylan@grepp.co>
2024-12-10 23:36:04 +0900

    implement basic webcam preview

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
-console.log("hello world");
+(async () => {
+  const stream = await navigator.mediaDevices.getUserMedia({
+    audio: false,
+    video: true,
+  });
+  const video = Object.assign(document.createElement("video"), {
+    autoplay: true,
+    srcObject: stream,
+  });
+  document.body.appendChild(video);
+})();
