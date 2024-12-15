2b331d223ddd63bf62c490c0ac30de139cb39175
Dylan (Jeongtae Kim) <dylan@grepp.co>
2024-12-15 12:58:40 +0900

    pick iphone camera

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 (async () => {
+  const devices = await navigator.mediaDevices.enumerateDevices();
+  const device = devices.find(({ label }) => label.includes("iPhone") && label.includes("카메라"));
   const stream = await navigator.mediaDevices.getUserMedia({
     audio: false,
-    video: true,
+    video: {
+      deviceId: device?.deviceId,
+    },
   });
   const video = Object.assign(document.createElement("video"), {
     autoplay: true,
