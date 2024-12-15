6f8a79e486a9c9e77bbf87b65459ea488a1cb284
Dylan (Jeongtae Kim) <dylan@grepp.co>
2024-12-15 23:36:26 +0900

    improve controls margin style

--- src/App.svelte
+++ src/App.svelte
@@ -110,15 +110,15 @@
 
   .controls {
     position: fixed;
-    top: 0;
-    left: 0;
+    top: 0.5rem;
+    left: 0.5rem;
+    right: 0.5rem;
     display: flex;
     flex-wrap: wrap;
-    gap: 1.5rem;
+    gap: 0.5rem 1.5rem;
     background: rgba(0, 0, 0, 0.5);
     width: fit-content;
     padding: 0.5rem;
-    margin: 0.5rem;
     border-radius: 0.5rem;
     color: white;
   }
